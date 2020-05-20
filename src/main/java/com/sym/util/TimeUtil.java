fd318c1ee017f2dbaf25a4ecf09320b8a4abcbaa
shenym <542569199@qq.com>
2020-05-20 17:17:04 +0800

    计时工具

--- src/main/java/com/sym/util/TimeUtil.java
+++ src/main/java/com/sym/util/TimeUtil.java
@@ -1,7 +1,11 @@
 package com.sym.util;
 
+import lombok.SneakyThrows;
+
+import java.util.function.Supplier;
+
 /**
- * 线程安全的计时工具
+ * 计时工具
  *
  * @author shenym
  * @date 2020/3/26 18:37
@@ -16,18 +20,37 @@ public class TimeUtil {
      * 同步执行 描述
      */
     public static void execute(String description, Runnable task) {
+        timing(description, task, null);
+    }
+
+    /**
+     * 同步执行 描述
+     */
+    @SneakyThrows
+    public static <T> void execute(String description, Supplier<T> supplier) {
+        timing(description, null, supplier);
+    }
+
+    private static <T> void timing(String description, Runnable r, Supplier<T> supplier){
         long start = System.nanoTime();
-        task.run();
+        T t = null;
+        if(null != r){
+            r.run();
+        }
+        if(null != supplier){
+            t = supplier.get();
+        }
         long end = System.nanoTime();
-        // 纳秒
+        // 纳秒、毫秒、秒
         long ns = end - start;
-        // 毫秒
         long ms = ns / NS_TO_MS;
-        // 秒
         long s = ms / MS_TO_S;
         System.out.println(description);
-        System.out.println("耗时：" + ns + "ns, " + ms + "ms, " + s + "s");
-        System.out.println("*************************");
+        if(null != t){
+            System.out.println("[执行结果]：" + t);
+        }
+        System.out.println("[消耗时间]：" + ns + "ns, " + ms + "ms, " + s + "s");
+        System.out.println("**********************************************");
         System.out.println();
     }
 }
