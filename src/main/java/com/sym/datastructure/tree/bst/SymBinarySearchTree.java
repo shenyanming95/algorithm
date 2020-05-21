e6e7779c685ca533532c3a8b6de6c9af446e05d8
shenyanming <sym542569199@gmail.com>
2020-05-21 22:52:26 +0800

    二叉搜索树

---
+++ src/main/java/com/sym/datastructure/tree/bst/SymBinarySearchTree.java
@@ -0,0 +1,41 @@
+package com.sym.datastructure.tree.bst;
+
+/**
+ * 二叉搜索树的链表实现
+ *
+ * @author shenyanming
+ * @date 2020/5/21 22:51.
+ */
+
+public class SymBinarySearchTree<E> implements IBinarySearchTree<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public void add(E e) {
+
+    }
+
+    @Override
+    public void remove(E e) {
+
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return false;
+    }
+
+}
