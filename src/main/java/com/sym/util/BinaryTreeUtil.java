6d844b7c536a1a0c82826b01aaa7094660b1df97
沈燕明 <542569199@qq.com>
2020-06-23 17:00:37 +0800

    工具类

--- src/main/java/com/sym/util/BinaryTreeUtil.java
+++ src/main/java/com/sym/util/BinaryTreeUtil.java
@@ -4,12 +4,19 @@ import com.sym.structure.tree.avl.AvlTree;
 import com.sym.structure.tree.bst.BinarySearchTree;
 
 /**
+ * 二叉树工具类
+ *
  * @author shenyanming
  * @date 2020/5/31 15:01.
  */
 
 public class BinaryTreeUtil {
 
+    /**
+     * 创建二叉搜索树
+     * @param nodeString 节点串
+     * @return
+     */
     public static BinarySearchTree<Integer> newBinarySearchTree(String nodeString) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<>();
         String[] nodeArray = nodeString.split(",");
@@ -17,11 +24,16 @@ public class BinaryTreeUtil {
             if ("".equals(nodeValue)) {
                 continue;
             }
-            bst.add(Integer.parseInt(nodeValue));
+            bst.add(Integer.parseInt(nodeValue.replace(" ", "")));
         }
         return bst;
     }
 
+    /**
+     * 创建AVL树
+     * @param nodeString 节点串
+     * @return
+     */
     public static AvlTree<Integer> newAvlTree(String nodeString) {
         AvlTree<Integer> avl = new AvlTree<>();
         String[] nodeArray = nodeString.split(",");
@@ -29,7 +41,7 @@ public class BinaryTreeUtil {
             if ("".equals(nodeValue)) {
                 continue;
             }
-            avl.add(Integer.parseInt(nodeValue));
+            avl.add(Integer.parseInt(nodeValue.replace(" ", "")));
         }
         return avl;
     }
