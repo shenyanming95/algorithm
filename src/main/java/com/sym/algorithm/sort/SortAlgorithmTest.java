c579f80cdf6aff964d19ee541bd6059095f00067
沈燕明 <542569199@qq.com>
2020-07-06 16:27:57 +0800

    排序算法测试

---
+++ src/main/java/com/sym/algorithm/sort/SortAlgorithmTest.java
@@ -0,0 +1,31 @@
+package com.sym.algorithm.sort;
+
+import com.sym.util.SymArrayUtil;
+import com.sym.util.TimeUtil;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+
+/**
+ * 比较各排序算法耗费的时间
+ *
+ * @author user
+ */
+public class SortAlgorithmTest {
+
+    @Test
+    public void test() {
+        // 待排序数组
+        int[] arr1 = SymArrayUtil.getRandomArray(12580);
+        int[] arr2 = Arrays.copyOf(arr1, arr1.length);
+        int[] arr3 = Arrays.copyOf(arr1, arr1.length);
+        int[] arr4 = Arrays.copyOf(arr1, arr1.length);
+        // 测试
+        TimeUtil.execute("冒泡排序", () -> BubbleSort.sort(arr1));
+        TimeUtil.execute("选择排序", () -> SelectSort.sort(arr2));
+        TimeUtil.execute("快速排序", () -> QuickSort.sort(arr3));
+        TimeUtil.execute("插入排序", () -> InsertSort.sort(arr4));
+
+    }
+}
---
+++ src/test/java/com/sym/MainTest.java
@@ -0,0 +1,8 @@
+package com.sym;
+
+/**
+ * @author shenyanming
+ * Created on 2020/7/6 16:27
+ */
+public class MainTest {
+}
