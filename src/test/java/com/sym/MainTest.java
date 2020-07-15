ee417e122e682843a2fd2c8cacb2b8723521039b
沈燕明 <542569199@qq.com>
2020-07-15 10:50:54 +0800

    测试类

--- src/test/java/com/sym/MainTest.java
+++ src/test/java/com/sym/MainTest.java
@@ -1,8 +1,14 @@
 package com.sym;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * @author shenyanming
  * Created on 2020/7/6 16:27
  */
+@Slf4j
 public class MainTest {
+    public static void main(String[] args) {
+
+    }
 }
