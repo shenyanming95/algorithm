4cc203a17e335490f7917880df919db9ec7dd063
shenyanming <sym542569199@gmail.com>
2020-07-26 21:35:23 +0800

    链哈希表

---
+++ src/main/java/com/sym/structure/map/linked/LinkedHashMap.java
@@ -0,0 +1,12 @@
+package com.sym.structure.map.linked;
+
+/**
+ * LinkedHashMap的节点继承自{@link com.sym.structure.map.hash.HashMap}的节点, 然后它加入了两个
+ * 链表指针prev和next, 通过这两个指针维护元素的添加顺序, 其它情况就和哈希表一样.
+ *
+ * @author shenyanming
+ * @date 2020/7/26 8:40.
+ */
+
+public class LinkedHashMap {
+}
