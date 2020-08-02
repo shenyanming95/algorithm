930c2ab1b38ba1b6b0e2c9d519f822b2a10aaf55
shenyanming <sym542569199@gmail.com>
2020-08-02 21:28:37 +0800

    字典树

---
+++ src/main/java/com/sym/structure/trie/ITrie.java
@@ -0,0 +1,64 @@
+package com.sym.structure.trie;
+
+/**
+ * 字典树, 也称为前缀树, 它会将字符串按字符分开, 依次保存在内部的节点上,
+ * 每次搜索字符串的时候, 可以根据字符确定字符串前缀是否依次匹配. 所以
+ * Trie, 适用于需要使用字符串前缀搜索的场景.
+ *
+ * @author shenyanming
+ * @date 2020/8/1 15:57.
+ */
+
+public interface ITrie<E> {
+
+    /**
+     * 字典树的元素数量
+     *
+     * @return 元素数量
+     */
+    int size();
+
+    /**
+     * 是否为空
+     *
+     * @return true-为空
+     */
+    boolean isEmpty();
+
+    /**
+     * 清空字典树
+     */
+    void clear();
+
+    /**
+     * 判断字典树是否包含某个字符串
+     *
+     * @param key 待匹配的字符串
+     * @return true-包含
+     */
+    boolean contains(String key);
+
+    /**
+     * 添加元素到字典树中
+     *
+     * @param key   字符串
+     * @param value 具体指
+     * @return 旧值, 可能为null
+     */
+    E add(String key, E value);
+
+    /**
+     * 删除在字典树上的字符串
+     *
+     * @param key 待匹配的字符串
+     */
+    void remove(String key);
+
+    /**
+     * 给定一个字符串前缀, 判断此前缀是否存在于字典树中
+     *
+     * @param prefix 字符串前缀
+     * @return true-存在
+     */
+    boolean startWith(String prefix);
+}
