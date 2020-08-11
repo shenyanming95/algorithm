f5153f28ea76bfaef7c1ad1b35e01b4ad93e1484
沈燕明 <542569199@qq.com>
2020-08-11 15:52:37 +0800

    队列接口

--- src/main/java/com/sym/structure/queue/IQueue.java
+++ src/main/java/com/sym/structure/queue/IQueue.java
@@ -2,42 +2,50 @@ package com.sym.structure.queue;
 
 /**
  * 队列接口
+ *
+ * @author shenyanming
+ * Created on 2020/3/6
  */
 public interface IQueue<E> {
 
-	/**
-	 * 队列置空
-	 */
-	void clear();
+    /**
+     * 队列置空
+     */
+    void clear();
 
-	/**
-	 * 判断队列是否为空
-	 * @return true-表示空队列
-	 */
-	boolean isEmpty();
+    /**
+     * 判断队列是否为空
+     *
+     * @return true-表示空队列
+     */
+    boolean isEmpty();
 
-	/**
-	 * 返回队列的长度
-	 * @return 队列长度
-	 */
-	int size();
+    /**
+     * 返回队列的长度
+     *
+     * @return 队列长度
+     */
+    int size();
 
-	/**
-	 * 获取队首元素,不会执行出队操作
-	 * @return 队首元素
-	 */
-	E peek();
+    /**
+     * 获取队首元素,不会执行出队操作
+     *
+     * @return 队首元素
+     */
+    E peek();
 
-	/**
-	 * 入队操作
-	 * @param e 数据
-	 */
-	void offer(E e);
+    /**
+     * 入队操作
+     *
+     * @param e 数据
+     */
+    void offer(E e);
 
-	/**
-	 * 出队操作
-	 * @return 队首元素
-	 */
-	E poll();
+    /**
+     * 出队操作
+     *
+     * @return 队首元素
+     */
+    E poll();
 
 }
