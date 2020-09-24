3d5e37615c2e3baae95414360a463bff1d9858f8
沈燕明 <542569199@qq.com>
2020-09-24 14:46:50 +0800

    插入排序

--- src/main/java/com/sym/algorithm/sort/impl/InsertionSort.java
+++ src/main/java/com/sym/algorithm/sort/impl/InsertionSort.java
@@ -1,22 +1,35 @@
 package com.sym.algorithm.sort.impl;
 
 import com.sym.algorithm.sort.AbstractIntegerSort;
+import com.sym.util.SymArrayUtil;
 
 /**
- * 插入排序
+ * 插入排序.
  * <p>
- * 取出数组中的元素i, 将i与i左边元素按照从右往左的顺序依次比较，若遇到比i大的元素j, 将i与j互换位置,
- * 否则继续比较直至遇到比i小的元素或达到数组头元素停止. 重复此过程:
- * 第一次循环：取到下标为1的元素，与其左边元素(下标为0)比较，若大于它则结束第一次循环，若小于它则互换他两的位置
- * 第二次循环：取到下标为2的元素，与其左边元素(下标为1,0)依次比较，若大于下标为1的元素则结束第二次循环，若小于下标为1的元素，互换他两位置；比较下标为1和下标为0的元素大小;
- * 第三次循环：取到下标为3的元素，与其左边元素(下标为2,1,0)依次比较...若大于下标为2的元素则结束第三次循环，若小于则互换他两的位置，比较下标为2和1的元素大小,...以此类推.
- * ...
- * 第n-1次循环，取到下标为(n-2,n-3,...,0)的元素，依次比较...
+ * 取出数组中的元素i, 将i与i左边元素按照从右往左的顺序依次比较，若遇到比i大的元素j,
+ * 将i与j互换位置, 否则继续比较直至遇到比i小的元素或达到数组头元素停止, 重复此过程.
+ * <pre>
+ *     第一次循环：取到下标为1的元素a，与其左边元素(下标为0)b比较, 若a > b则结束第一次循环, 若a < b则互换位置;
+ *     第二次循环：取到下标为2的元素c, 与其左边元素(下标为1,0)a和b依次比较，若c > a则结束第二次循环, 若c < a则互换c和a位置; 再接着比较c和b;
+ *     ...
+ *     第n-1次循环，取到下标为(n-2,n-3,...,0)的元素，依次比较...
+ * </pre>
+ * 如果原数组中逆序对较少, 插入排序的效率很高, 反之效率较低.
  *
  * @author shenyanming
  * Created on 2020/9/2 11:59
  */
 public class InsertionSort extends AbstractIntegerSort {
+
+    public static void main(String[] args) {
+        int[] array = SymArrayUtil.getRandomArray(11);
+        SymArrayUtil.print(array);
+
+        InsertionSort sort = new InsertionSort(array);
+        sort.sort();
+
+        SymArrayUtil.print(array);
+    }
 
     public InsertionSort(int[] array) {
         super(array, "插入排序");
@@ -24,18 +37,14 @@ public class InsertionSort extends AbstractIntegerSort {
 
     @Override
     protected void internalSort(int[] array) {
-        int start;
+        // 插入排序的第一个元素不需要比较, 所以直接从下标为1的元素开始
         for (int i = 1, len = array.length; i < len; i++) {
-            start = i;
-            while (start > 0) {
-                // 依次将元素与其左边的各个元素比较
-                if (compareByIndex(start - 1, start) > 0) {
-                    swap(start, start - 1);
-                    start--;
-                } else {
-                    // 因为元素左边以前有序了，如果元素最右边都小于这个元素了，那么之前的元素肯定都小于它
-                    break;
-                }
+            int start = i;
+            // 依次与左边元素比较, 如果左边元素大, 则与左边元素互换位置.
+            // 由于数组左边元素已经有序, 如果遇到一个左边元素比当前元素小, 那么可以跳出循环
+            while (start > 0 && compareByIndex(start - 1, start) > 0) {
+                swap(start - 1, start);
+                start--;
             }
         }
     }
