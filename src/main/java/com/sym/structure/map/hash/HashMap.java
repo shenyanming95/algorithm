57ba75bf6a01b1d2e7a1d3d613e3ecff5cc11193
沈燕明 <542569199@qq.com>
2020-10-16 09:06:52 +0800

    HashMap

--- src/main/java/com/sym/structure/map/hash/HashMap.java
+++ src/main/java/com/sym/structure/map/hash/HashMap.java
@@ -4,7 +4,6 @@ import com.sym.structure.map.IMap;
 import com.sym.structure.map.linked.LinkedHashMap;
 import com.sym.structure.queue.IQueue;
 import com.sym.structure.queue.linked.LinkedQueue;
-
 import java.util.Objects;
 
 /**
@@ -314,7 +313,47 @@ public class HashMap<K, V> implements IMap<K, V> {
      * @param insertionEntry 新添加的Entry节点
      */
     private void afterAdd(Entry<K, V> insertionEntry) {
+        Entry<K, V> parent = insertionEntry.parent;
 
+        // 添加的是根节点 或者 上溢到达了根节点
+        if (parent == null) {
+            black(insertionEntry);
+            return;
+        }
+
+        // 如果父节点是黑色，直接返回
+        if (isBlack(parent)) return;
+
+        // 叔父节点
+        Entry<K, V> uncle = parent.sibling();
+        // 祖父节点
+        Entry<K, V> grand = red(parent.parent);
+        if (isRed(uncle)) { // 叔父节点是红色【B树节点上溢】
+            black(parent);
+            black(uncle);
+            // 把祖父节点当做是新添加的节点
+            afterAdd(grand);
+            return;
+        }
+
+        // 叔父节点不是红色
+        if (parent.isLeftChild()) { // L
+            if (insertionEntry.isLeftChild()) { // LL
+                black(parent);
+            } else { // LR
+                black(insertionEntry);
+                rotateLeft(parent);
+            }
+            rotateRight(grand);
+        } else { // R
+            if (insertionEntry.isLeftChild()) { // RL
+                black(insertionEntry);
+                rotateRight(parent);
+            } else { // RR
+                black(parent);
+            }
+            rotateLeft(grand);
+        }
     }
 
     /**
@@ -323,9 +362,118 @@ public class HashMap<K, V> implements IMap<K, V> {
      * @param removalEntry 真正被删除的Entry节点
      */
     private void afterRemove(Entry<K, V> removalEntry) {
+        // 如果删除的节点是红色
+        // 或者 用以取代删除节点的子节点是红色
+        if (isRed(removalEntry)) {
+            black(removalEntry);
+            return;
+        }
 
+        Entry<K, V> parent = removalEntry.parent;
+        if (parent == null) return;
+
+        // 删除的是黑色叶子节点【下溢】
+        // 判断被删除的node是左还是右
+        boolean left = parent.left == null || removalEntry.isLeftChild();
+        Entry<K, V> sibling = left ? parent.right : parent.left;
+        if (left) { // 被删除的节点在左边，兄弟节点在右边
+            if (isRed(sibling)) { // 兄弟节点是红色
+                black(sibling);
+                red(parent);
+                rotateLeft(parent);
+                // 更换兄弟
+                sibling = parent.right;
+            }
+
+            // 兄弟节点必然是黑色
+            if (isBlack(sibling.left) && isBlack(sibling.right)) {
+                // 兄弟节点没有1个红色子节点，父节点要向下跟兄弟节点合并
+                boolean parentBlack = isBlack(parent);
+                black(parent);
+                red(sibling);
+                if (parentBlack) {
+                    afterRemove(parent);
+                }
+            } else { // 兄弟节点至少有1个红色子节点，向兄弟节点借元素
+                // 兄弟节点的左边是黑色，兄弟要先旋转
+                if (isBlack(sibling.right)) {
+                    rotateRight(sibling);
+                    sibling = parent.right;
+                }
+
+                color(sibling, colorOf(parent));
+                black(sibling.right);
+                black(parent);
+                rotateLeft(parent);
+            }
+        } else { // 被删除的节点在右边，兄弟节点在左边
+            if (isRed(sibling)) { // 兄弟节点是红色
+                black(sibling);
+                red(parent);
+                rotateRight(parent);
+                // 更换兄弟
+                sibling = parent.left;
+            }
+
+            // 兄弟节点必然是黑色
+            if (isBlack(sibling.left) && isBlack(sibling.right)) {
+                // 兄弟节点没有1个红色子节点，父节点要向下跟兄弟节点合并
+                boolean parentBlack = isBlack(parent);
+                black(parent);
+                red(sibling);
+                if (parentBlack) {
+                    afterRemove(parent);
+                }
+            } else { // 兄弟节点至少有1个红色子节点，向兄弟节点借元素
+                // 兄弟节点的左边是黑色，兄弟要先旋转
+                if (isBlack(sibling.left)) {
+                    rotateLeft(sibling);
+                    sibling = parent.left;
+                }
+
+                color(sibling, colorOf(parent));
+                black(sibling.left);
+                black(parent);
+                rotateRight(parent);
+            }
+        }
     }
 
+    private void rotateLeft(Entry<K, V> grand) {
+        Entry<K, V> parent = grand.right;
+        Entry<K, V> child = parent.left;
+        grand.right = child;
+        parent.left = grand;
+        afterRotate(grand, parent, child);
+    }
+
+    private void rotateRight(Entry<K, V> grand) {
+        Entry<K, V> parent = grand.left;
+        Entry<K, V> child = parent.right;
+        grand.left = child;
+        parent.right = grand;
+        afterRotate(grand, parent, child);
+    }
+
+    private void afterRotate(Entry<K, V> grand, Entry<K, V> parent, Entry<K, V> child) {
+        // 让parent称为子树的根节点
+        parent.parent = grand.parent;
+        if (grand.isLeftChild()) {
+            grand.parent.left = parent;
+        } else if (grand.isRightChild()) {
+            grand.parent.right = parent;
+        } else { // grand是root节点
+            entryTable[index(grand.key)] = parent;
+        }
+
+        // 更新child的parent
+        if (child != null) {
+            child.parent = grand;
+        }
+
+        // 更新grand的parent
+        grand.parent = parent;
+    }
 
     /**
      * 寻找该key在此HashMap中对应的Entry
@@ -618,5 +766,31 @@ public class HashMap<K, V> implements IMap<K, V> {
         // 也是为了避免hash冲突
         int hash = key.hashCode();
         return hash ^ (hash >>> 16);
+    }
+
+    private Entry<K, V> color(Entry<K, V> entry, boolean color) {
+        if (entry == null) return entry;
+        entry.color = color;
+        return entry;
+    }
+
+    private Entry<K, V> red(Entry<K, V> entry) {
+        return color(entry, RED);
+    }
+
+    private Entry<K, V> black(Entry<K, V> entry) {
+        return color(entry, BLACK);
+    }
+
+    private boolean colorOf(Entry<K, V> entry) {
+        return entry == null ? BLACK : entry.color;
+    }
+
+    private boolean isBlack(Entry<K, V> entry) {
+        return colorOf(entry) == BLACK;
+    }
+
+    private boolean isRed(Entry<K, V> entry) {
+        return colorOf(entry) == RED;
     }
 }
